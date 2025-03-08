6dadf049da1f422f328e81ad7af68b402d37847a
maverixgg <139784869+maverixgg@users.noreply.github.com>
2025-03-08 17:01:41 +0600

    Add files via upload

---
+++ Week_1/task_1.py
@@ -0,0 +1,19 @@
+A = "ABRACADABRA"
+
+print(A[2])
+
+X = "the quick brown fox jumps over the lazy dog"
+print(X[10:19])
+
+P="আমার"
+Q="সোনার"
+R="বাংলা"
+
+Z = print(P+ " " +Q+ " "+R)
+
+singer_name = "Habib Wahid"
+song_name = "Bhalobashbo bashbo re bondhu"
+
+Y = f"My Favorite singer is {singer_name}, whose most popular song is {song_name}"
+print(Y)
+
